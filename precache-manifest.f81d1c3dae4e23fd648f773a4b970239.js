02e56f56f1c2200f819bed6a22a08b9eb00c25cf
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-19 10:07:02 +0300

    Updates

---
+++ precache-manifest.f81d1c3dae4e23fd648f773a4b970239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c6a8cd8c8372c18aab7ccc87c0c4e3",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "024f953ec02dedb08361",
+    "url": "/re-store/static/css/main.6c115338.chunk.css"
+  },
+  {
+    "revision": "9bc83c9a1733d7f348fe",
+    "url": "/re-store/static/js/2.c0905ca2.chunk.js"
+  },
+  {
+    "revision": "024f953ec02dedb08361",
+    "url": "/re-store/static/js/main.7892b0ba.chunk.js"
+  },
+  {
+    "revision": "5d6fe284d288fb554198",
+    "url": "/re-store/static/js/runtime~main.d45e52b3.js"
+  }
+]);
