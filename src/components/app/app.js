de3147638d7707737d90427e74463be14e31cc57
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-08-20 08:07:56 +0300

    Add HashRouter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HomePage, CartPage } from '../pages';
 import './app.css';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import ShopHeader from '../shop-header';
 
 const App = () => {
@@ -9,11 +9,12 @@ const App = () => {
   return (
       <main role="main" className="container">
         <ShopHeader numItems={5} total={100}/>
-        <Switch>
-          <Route path='/' render={() => <Redirect to="/re-store/" />} exact/>
-          <Route path='/re-store/' component={HomePage} exact/>
-          <Route path='/re-store/cart/' component={CartPage} />
-        </Switch>
+        <HashRouter >
+          <Switch>
+            <Route path='/' component={HomePage} exact/>
+            <Route path='/cart/' component={CartPage} />
+          </Switch>
+        </HashRouter>
       </main>
   );
 }
--- src/components/shop-header/shop-header.js
+++ src/components/shop-header/shop-header.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './shop-header.css';
-import { Link } from 'react-router-dom';
+import { HashRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const ShopHeader = ({ total, count }) => {
   
   return (
     <header className="shop-header row">
-      <Link to="/re-store/" >
-        <span className="logo text-dark">ReStore</span>
-      </Link>
-      <Link to="/re-store/cart/">
-        <span className="shopping-cart">
-          <i className="cart-icon fa fa-shopping-cart"/>
-          {count} items (${total})
-        </span>
-      </Link>
+      <HashRouter>
+        <Link to="/" >
+          <span className="logo text-dark">ReStore</span>
+        </Link>
+        <Link to="/cart/">
+          <span className="shopping-cart">
+            <i className="cart-icon fa fa-shopping-cart"/>
+            {count} items (${total})
+          </span>
+        </Link>
+      </HashRouter>
     </header>
   );
 }
